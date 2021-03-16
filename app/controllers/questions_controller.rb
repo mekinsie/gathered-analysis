289df11f65f5ec4ddef1b2bcf4c6ee274ea25bd1
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-16 11:20:32 -0700

    Add flash messages for forms.
    Co-authored-by: Andy Lawson <alawson89@gmail.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,6 +10,7 @@ def create
   @survey = Survey.find(params[:survey_id])
   @question = @survey.questions.new(question_params)
   if @question.save
+    flash[:notice] = "Question successfully added!"
     redirect_to survey_path(@survey)
   else
     render :new
@@ -25,6 +26,7 @@ end
 def update
   @question = Question.find(params[:id])
   if @question.update(question_params)
+    flash[:notice] = "Question successfully updated!"
     redirect_to survey_path(@question.survey)
   else
     @survey = Survey.find(params[:survey_id])
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -39,6 +39,7 @@ class SurveysController < ApplicationController
     # Code for updating an survey goes here.
     @survey = Survey.find([params[:id]])
     if @survey.update(survey_params)
+      flash[:notice] = "survey successfully updated!"
       redirect_to surveys_path
     else
       render :edit
